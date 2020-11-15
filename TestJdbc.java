de1d865481994be05c598b8a277dfd2900bd838c
Abhinay Koppera <kopperaabhinay@gmail.com>
2020-11-15 18:37:32 -0500

    first commit

---
+++ TestJdbc.java
@@ -0,0 +1,24 @@
+package abhi.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String jdbcurl="jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false";
+		String user="hbstudent";
+		String pass="hbstudent";
+		try {
+			System.out.println("connecting to database"+ jdbcurl);
+			Connection Conn=DriverManager.getConnection(jdbcurl,user,pass);
+			System.out.println("Connection Successful!!");
+		}
+		catch(Exception exc) {
+			exc.printStackTrace();
+		}
+
+	}
+
+}
